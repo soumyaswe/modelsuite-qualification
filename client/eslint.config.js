31b42a2edb3d11835ab7f97104f5b648be292352
soumya <soumya24work@gmail.com>
2026-06-30 15:27:03 +0530

    [27-26-15-5-4] fixed all lint errors

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -12,10 +13,23 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
     ],
+    plugins: { react },
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      // Recognise JSX usages so components used only in JSX are not flagged
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
